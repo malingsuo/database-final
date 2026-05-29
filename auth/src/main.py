eede0eab020c59fbec99184920bfcafa13320c8a
Daniel <dddanielliu@gmail.com>
2026-05-29 14:27:37 +0800

    update init structure

--- auth/src/main.py
+++ auth/src/main.py
@@ -16,7 +16,7 @@ from pydantic import BaseModel, ConfigDict
 # --- Configuration ---
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_USER = os.getenv("DB_USERNAME")
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
--- backend/src/main.py
+++ backend/src/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_USERNAME = os.getenv("DB_USERNAME", "postgres")
+DB_USERNAME = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_NAME = os.getenv("DB_NAME", "dbfinal")
 
